f0b74fa34599c223d19b8d76c5dcc3c2c5ad5934
NihilisticPandemonium <whiro@tukui.org>
2020-08-26 06:37:48 -0700

    Add initial support for Assembling Machine 7-10 from Miles Bobs Expansion.

--- data-updates.lua
+++ data-updates.lua
@@ -2,58 +2,134 @@ local item = data.raw.item
 
 bobmods.lib.tech.add_recipe_unlock("automation-6", "assembling-machine-6-bigger")
 
+local miles = mods["MilesBobsExpansion"] ~= nil
+
+local iter
+if miles then
+	iter = function(func)
+		for tier = 7, 10 do
+			func(tier)
+		end
+	end
+
+	iter(function(tier) bobmods.lib.tech.add_recipe_unlock("automation-"..tier, "assembling-machine-"..tier.."-bigger") end)
+end
+
 if item["nitinol-alloy"] then
   bobmods.lib.recipe.replace_ingredient("assembling-machine-6-bigger", "steel-plate", "nitinol-alloy")
   bobmods.lib.tech.add_prerequisite("automation-6", "nitinol-processing")
+  if miles then
+	iter(function(tier)
+		bobmods.lib.recipe.replace_ingredient("assembling-machine-"..tier.."-bigger", "steel-plate", "nitinol-alloy")
+		bobmods.lib.tech.add_prerequisite("automation-"..tier, "nitinol-processing")
+	end)
+end
 else
   if item["titanium-plate"] then
     bobmods.lib.recipe.replace_ingredient("assembling-machine-6-bigger", "steel-plate", "titanium-plate")
     bobmods.lib.tech.add_prerequisite("automation-6", "titanium-processing")
+	if miles then
+		iter(function(tier)
+			bobmods.lib.recipe.replace_ingredient("assembling-machine-"..tier.."-bigger", "steel-plate", "titanium-plate")
+			bobmods.lib.tech.add_prerequisite("automation-"..tier, "titanium-processing")
+		end)
+	end
   end
 end
 
 if item["tungsten-carbide"] then
   bobmods.lib.recipe.replace_ingredient("assembling-machine-6-bigger", "iron-plate", "tungsten-carbide")
   bobmods.lib.tech.add_prerequisite("automation-6", "tungsten-alloy-processing")
+  if miles then
+	iter(function(tier)
+		bobmods.lib.recipe.replace_ingredient("assembling-machine-"..tier.."-bigger", "iron-plate", "tungsten-carbide")
+		bobmods.lib.tech.add_prerequisite("automation-"..tier, "tungsten-alloy-processing")
+	end)
+  end
 else
   if item["tungsten-plate"] then
     bobmods.lib.recipe.replace_ingredient("assembling-machine-6-bigger", "iron-plate", "tungsten-plate")
     bobmods.lib.tech.add_prerequisite("automation-6", "tungsten-processing")
+	if miles then
+		iter(function(tier)
+			bobmods.lib.recipe.replace_ingredient("assembling-machine-"..tier.."-bigger", "iron-plate", "tungsten-plate")
+			bobmods.lib.tech.add_prerequisite("automation-"..tier, "tungsten-processing")
+		end)
+	end
   end
 end
 
 if item["nitinol-bearing"] then
   bobmods.lib.recipe.add_ingredient("assembling-machine-6-bigger", {"nitinol-bearing", 5})
+  if miles then
+	iter(function(tier)
+		bobmods.lib.recipe.add_ingredient("assembling-machine-"..tier.."-bigger", {"nitinol-bearing", 5 + (5 * (tier - 6))})
+	end)
+  end
 else
   if item["titanium-bearing"] then
     bobmods.lib.recipe.add_ingredient("assembling-machine-6-bigger", {"titanium-bearing", 5})
   else
     bobmods.lib.recipe.add_ingredient("assembling-machine-6-bigger", {"iron-gear-wheel", 3})
   end
+  if miles then
+	iter(function(tier)
+		bobmods.lib.recipe.add_ingredient("assembling-machine-"..tier.."-bigger", {"titanium-bearing", 5 + (5 * (tier - 6))})
+	end)
+  end
 end
 
 if item["nitinol-gear-wheel"] then
   bobmods.lib.recipe.replace_ingredient("assembling-machine-6-bigger", "iron-gear-wheel", "nitinol-gear-wheel")
   bobmods.lib.tech.add_prerequisite("automation-6", "nitinol-processing")
+  if miles then
+	iter(function(tier)
+		bobmods.lib.recipe.replace_ingredient("assembling-machine-"..tier.."-bigger", "iron-gear-wheel", "nitinol-gear-wheel")
+		bobmods.lib.tech.add_prerequisite("automation-"..tier, "nitinol-processing")
+	end)
+  end
 else
   if item["titanium-gear-wheel"] then
     bobmods.lib.recipe.replace_ingredient("assembling-machine-6-bigger", "iron-gear-wheel", "titanium-gear-wheel")
     bobmods.lib.tech.add_prerequisite("automation-6", "titanium-processing")
+	if miles then
+		iter(function(tier)
+			 bobmods.lib.recipe.replace_ingredient("assembling-machine-"..tier.."-bigger", "iron-gear-wheel", "titanium-gear-wheel")
+    		 bobmods.lib.tech.add_prerequisite("automation-"..tier, "titanium-processing")
+		end)
+	end
   else
     if item["tungsten-gear-wheel"] then
       bobmods.lib.recipe.replace_ingredient("assembling-machine-6-bigger", "iron-gear-wheel", "tungsten-gear-wheel")
       bobmods.lib.tech.add_prerequisite("automation-6", "tungsten-processing")
     end
+	if miles then
+		iter(function(tier)
+			 bobmods.lib.recipe.replace_ingredient("assembling-machine-"..tier.."-bigger", "iron-gear-wheel", "tungsten-gear-wheel")
+     		 bobmods.lib.tech.add_prerequisite("automation-"..tier, "tungsten-processing")
+		end)
+	end
   end
 end
 
 if item["advanced-processing-unit"] then
   bobmods.lib.recipe.replace_ingredient("assembling-machine-6-bigger", "processing-unit", "advanced-processing-unit")
   bobmods.lib.tech.add_prerequisite("automation-6-bigger", "advanced-electronics-3")
+  if miles then
+	iter(function(tier)
+		bobmods.lib.recipe.replace_ingredient("assembling-machine-"..tier.."-bigger", "processing-unit", "advanced-processing-unit")
+  		bobmods.lib.tech.add_prerequisite("automation-"..tier.."-bigger", "advanced-electronics-3")
+	end)
+  end
 end
 
 if settings.startup["bobmods-assembly-limits"].value == true then
   data.raw["assembling-machine"]["assembling-machine-6-bigger"].ingredient_count = 16
+  if miles then
+	iter(function(tier)
+		data.raw["assembling-machine"]["assembling-machine-"..tier.."-bigger"].ingredient_count = 16 + (4 * (tier - 6))
+	end)
+  end
 end
 
 if mods["ShinyBobGFX"] then
